76db980aacacccf218f0530815868f4b7b93ebd8
Dirceu Vieira <dirceu@vimond.com>
2016-03-18 22:53:45 -0300

    DJs controller

---
+++ db/migrate/20160319004653_create_djs.rb
@@ -0,0 +1,14 @@
+class CreateDjs < ActiveRecord::Migration
+  def self.up
+    create_table :djs do |t|
+      t.integer :id,
+      t.string :name
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :djs
+  end
+end
